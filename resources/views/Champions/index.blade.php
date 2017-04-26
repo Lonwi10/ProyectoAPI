dbe2483824822545d31751e53091abf7dc047fd4
Lonwi10 <lonwi.95@gmail.com>
2017-04-26 19:47:38 +0200

    Retocando un poco

--- resources/views/Champions/index.blade.php
+++ resources/views/Champions/index.blade.php
@@ -2,5 +2,21 @@
 
 
 @section('content')
-	Helloooooo
+	<div class="row">
+	  <div class="col-md-4">
+		<a href="{{url('/grafico1')}}">
+			<img border="0" alt="Grafico de Barras" src="grafico1.jpg" width="300" height="300">
+		</a>
+	  </div>
+	  <div class="col-md-4">
+	  	<a href="{{url('/grafico2')}}">
+			<img border="0" alt="Busqueda" src="grafico2.jpg" width="300" height="300">
+		</a>
+	  </div>
+	  <div class="col-md-4">
+	  	<a href="{{url('/externa')}}">
+			<img border="0" alt="Api Externa" src="grafico3.jpg" width="300" height="300">
+		</a>
+	  </div>
+	</div>
 @endsection
